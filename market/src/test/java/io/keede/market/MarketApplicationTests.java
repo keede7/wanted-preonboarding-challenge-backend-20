5ed715a9496698c0f0c7a3dc5f7d2ccc3d9ae27c
keede <wesaq11@gmail.com>
2024-05-28 11:49:23 +0900

    feat: add Sample Project

---
+++ market/src/test/java/io/keede/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.keede.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
